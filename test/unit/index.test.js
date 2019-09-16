9ac00a9ed0bf229bbfc78b8b7b9fd7229d26cc7c
Sam Boylett <sam.boylett@netsells.co.uk>
2019-09-16 16:44:31 +0100

    Add test using actual HTML

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,14 +1,22 @@
 import SimulateUser from '../../src/index';
+import app from './app.html';
 
 describe('SimulateUser', () => {
     beforeEach(() => {
-        document.innerHTML = '';
+        document.innerHTML = app;
     });
 
     describe('constructor', () => {
         it('uses document as default node', () => {
             const user = new SimulateUser();
             expect(user.node).toBe(document);
+        });
+
+        it('can use any element as a node', () => {
+            const node = document.querySelector('td');
+
+            const user = new SimulateUser(node);
+            expect(user.node).toBe(node);
         });
     });
 });
